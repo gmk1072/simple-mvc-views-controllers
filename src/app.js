9e1becf4104176d909f3a3dceb1d2d1e89cf14c3
Cody Van De Mark <cavigm@rit.edu>
2018-02-21 12:55:39 -0500

    updated with comments

--- src/app.js
+++ src/app.js
@@ -1,17 +1,69 @@
+// import libraries
+// path is a built-in node library to handle file system paths
 const path = require('path');
+// express is a popular Model-View-Controller framework for Node
 const express = require('express');
+// compression library to gzip responses for smaller/faster transfer
 const compression = require('compression');
+// favicon library to handle favicon requests
 const favicon = require('serve-favicon');
+// Library to parse cookies from the requests
 const cookieParser = require('cookie-parser');
+// library to handle POST requests any information sent in an HTTP body
 const bodyParser = require('body-parser');
 
-const router = require('./router.js');
+// In MVC, you have 'routes' that line up URLs to controller methods
+const router = require('./router.js'); // import our router.js file to handle the MVC routes
 
+// Port set by process.env.PORT environment variable.
+// If the process.env.PORT variable or the env.NODE_PORT variables do not exist, use port 3000
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+// call express to get an Express MVC server object
 const app = express();
 
+// app.use tells express to use different options
+// This option tells express to use /assets in a URL path as a static mirror to our client folder
+// Any requests to /assets will map to the client folder to find a file
+// For example going to /assets/img/favicon.png would return the favicon image
 app.use('/assets', express.static(path.resolve(`${__dirname}/../client/`)));
+
+// Call compression and tell the app to use it
 app.use(compression());
 
+// parse form POST requests as application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json body requests. These are usually POST requests or
+// requests with a body parameter in AJAX
+// Alternatively, this might be a web API request from a mobile app,
+// another server or another application
+app.use(bodyParser.json());
+
+// app.set sets one of the express config options
+// set up the view (V of MVC) to use jade (not shown in this example but needed for express to work)
+// You can use other view engines besides jade
+app.set('view engine', 'jade');
+
+// set the views path to the template directory
+// (not shown in this example but needed for express to work)
+app.set('views', `${__dirname}/../views`);
+
+// call favicon with the favicon path and tell the app to use it
+app.use(favicon(`${__dirname}/../client/img/favicon.png`));
+
+// call the cookie parser library and tell express to use it
+app.use(cookieParser());
+
+// pass our app to our router object to map the routes
+router(app);
+
+// Tell the app to listen on the specified port
+app.listen(port, (err) => {
+    // if the app fails, throw the err
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
+});
 
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,17 +1,87 @@
-const path = require('path');
+const path = require('path'); // path is a built-in node library to handle file system paths
 
-// There is no reason for the name here except an arbitary example
-// of updating the server data based on a client request.
-let name = 'unknown';
+// There is no reason for the name here except as an example of how to set something for the POST
+let name = 'unknown'; // We will set and get an arbitrary name
 
+// function to handle requests to the main page
+// controller functions in Express receive the full HTTP request
+// and a pre-filled out response object to send
 const hostIndex = (req, res) => {
+  // path.resolve finds the absolute path for you
+  // send the main.page.html file using a resolved absolute path
+  // the res.sendFile action returns a file to a user
+  // res.sendFile response to the HTTP request so you can't send any more data through HTTP
+  // until the next request
   res.sendFile(path.resolve(`${__dirname}/../../views/index.html`));
 };
 
+// function to handle requests to the gallery page
+// controller functions in Express receive the full HTTP request
+// and a pre-filled out response object to send
 const hostPage1 = (req, res) => {
+  // path.resolve finds the absolute path for you
+  // send the gallery.html file using a resolved absolute path
+  // the res.sendFile action returns a file to a user
+  // res.sendFile response to the HTTP request so you can't send any more data through HTTP
+  // until the next request
   res.sendFile(path.resolve(`${__dirname}/../../views/page1.html`));
 };
 
+// function to handle requests to the gallery page
+// controller functions in Express receive the full HTTP request
+// and a pre-filled out response object to send
 const hostPage2 = (req, res) => {
+  // path.resolve finds the absolute path for you
+  // send the gallery.html file using a resolved absolute path
+  // the res.sendFile action returns a file to a user
+  // res.sendFile response to the HTTP request so you can't send any more data through HTTP
+  // until the next request
   res.sendFile(path.resolve(`${__dirname}/../../views/page2.html`));
+};
+
+// function to handle get request to send the name
+// controller functions in Express receive the full HTTP request
+// and a pre-filled out response object to send
+const getName = (req, res) => {
+  // res.json returns json to the page. Since this sends back the data through HTTP
+  // you can't send any more data to this user until the next response
+  res.json({ name });
+};
+
+// function to handle a request to set the name
+// controller functions in Express receive the full HTTP request
+// and get a pre-filled out response object to send
+// ADDITIONALLY, with body-parser we will get the body/form/POST data in the request as req.body
+const setName = (req, res) => {
+  // check if the required fields exist
+  // normally you would also perform validation to know if the data they sent you was real
+  if (!req.body.firstname || !req.body.lastname) {
+    // if not respond with a 400 error
+    // (either through json or a web page depending on the client dev)
+    return res.status(400).json({ error: 'firstname and lastname are both required' });
+  }
+
+  // if required fields are good, then set name
+  name = `${req.body.firstname} ${req.body.lastname}`;
+
+  // respond with our new name updated. This could just be a 200 response to say the name was good
+  // but we will also throw in the json of the new name just for convenience
+  return res.json({ name });
+};
+
+// function to handle a request to any non-real resources (404)
+// controller functions in Express receive the full HTTP request
+// and get a pre-filled out response object to send
+const notFound = (req, res) => {
+  res.status(404).sendFile(path.resolve(`${__dirname}/../../views/notFound.html`));
+};
+
+// export the relevant public controller functions
+module.exports = {
+  index: hostIndex,
+  page1: hostPage1,
+  page2: hostPage2,
+  getName,
+  setName,
+  notFound,
 };
